6256c2f2ca7e1369340dcfc924c048e8133b2b87
Jeffrey Thomas <jeffreythomas10566x@gmail.com>
2025-10-07 07:00:53 +0530

    Customizing OAuth Login Popup with Prompt and Login Hint

--- services/googleAuthService.js
+++ services/googleAuthService.js
@@ -11,9 +11,16 @@ const client = new OAuth2Client({
 
 export function generateGoogleAuthUrl() {
   return client.generateAuthUrl({
-    scope : [ "email" , "profile" , "openid" ]
+    scope : [ "email" , "profile" , "openid" ],
+    prompt : "consent",      // concent / select_account / none
+    login_hint : "jeffreythomas10566x@gmail.com" // by default selects this account 
   })
 }
+// prompt: "consent" → Always show the consent screen again (used to get new permissions or a refresh token).
+
+// prompt: "select_account" → Always show the account chooser (lets the user pick which Google account to use).
+
+// prompt: "none" → No UI shown; silent login attempt (fails if user interaction is needed). only if there is 1 acc / if not then error 
 
 export async function fetchUserFromGoogle(code) {
   console.log("Running fetchIdToken function...");
